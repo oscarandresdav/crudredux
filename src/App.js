9e0eb343bdc07b6bfe6ec76d5412145a32bdb033
“Oscar <oscar7822002@gmail.com>
2022-07-13 18:00:04 -0500

    Configurando REDUX, instalando dev tools y creando el store

--- src/App.js
+++ src/App.js
@@ -5,17 +5,23 @@ import Productos from './components/Productos';
 import NuevoProducto from './components/NuevoProducto';
 import EditarProducto from './components/EditarProducto';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 function App() {
   return (
     <Router>
-      <Header />
-      <div className="container mt-5">
-        <Routes>
-          <Route path='/' element={<Productos />} />
-          <Route path='/productos/nuevo' element={<NuevoProducto />} />
-          <Route path='/productos/editar/:id' element={<EditarProducto />} />
-        </Routes>
-      </div>
+      <Provider store={store}>
+        <Header />
+        <div className="container mt-5">
+          <Routes>
+            <Route path='/' element={<Productos />} />
+            <Route path='/productos/nuevo' element={<NuevoProducto />} />
+            <Route path='/productos/editar/:id' element={<EditarProducto />} />
+          </Routes>
+        </div>
+      </Provider>
     </Router>
   );
 }
