9c9f3f69177913164ca0501095dce74a7be564c2
“Oscar <oscar7822002@gmail.com>
2022-07-13 10:12:41 -0500

    Agregando React Router DOM y Componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header';
+import Productos from './components/Productos';
+import NuevoProducto from './components/NuevoProducto';
+import EditarProducto from './components/EditarProducto';
 
 function App() {
   return (
-    <Fragment>
+    <Router>
       <Header />
-    </Fragment>
+      <div className="container mt-5">
+        <Routes>
+          <Route path='/' element={<Productos />} />
+          <Route path='/productos/nuevo' element={<NuevoProducto />} />
+          <Route path='/productos/editar/:id' element={<EditarProducto />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
